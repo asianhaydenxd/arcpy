dc03affb3c140edc0f234687591f40a04c037639
Hayden <asianhaydenxd@gmail.com>
2023-09-16 01:50:04 -0400

    update repl

--- src/repl.py
+++ src/repl.py
@@ -1,3 +1,5 @@
+import lex, parse, eval
+
 class REPL:
     def __init__(self):
         self.knowns = []
@@ -5,8 +7,17 @@ class REPL:
     def start(self):
         print("arcpy v.1.0.0")
         while True:
-            cmd = input("> ")
-            if cmd == ":q":
+            code = input("> ")
+
+            if code == ":q":
                 print("Quitting...")
                 break
+
+            lexer = lex.Lexer(code)
+            tokens = lexer.lex()
+            parser = parse.Parser(tokens)
+            expression = parser.parse()
+            evaluator = eval.Evaluator(expression)
+            value = evaluator.evaluate()
+            print(value)
             
